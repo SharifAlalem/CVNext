8a9f4b2b2f643f5c1bf9e724e83a9979121161d7
Sharif Alalem <sharif.alalem@savings-united.com>
2024-08-12 02:45:33 +0200

    fix unused data

--- src/components/PersonalInfos/personalInfos.tsx
+++ src/components/PersonalInfos/personalInfos.tsx
@@ -2,7 +2,7 @@ import BaseForm from "../ui/BaseForm/baseForm";
 import { useFormBuildData } from "@/context/formBuildData";
 
 const PersonalInfos = () => {
-    const {data,setData} = useFormBuildData();
+    const {data} = useFormBuildData();
 
     return ( 
         <BaseForm data={data}><h1>Personal Information</h1></BaseForm>
